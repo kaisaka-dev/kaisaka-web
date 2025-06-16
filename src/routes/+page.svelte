4cff8a11b299ccfa4f78f0d494256336b07af2cd
Shoufou <riverap1231@gmail.com>
2025-06-16 22:30:56 +0800

    added login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<body class = "bg-[url(/img/bgimage.png)] bg-cover bg-position-[center_top_-5rem]">
+   <div class = "mx-55 absolute inset-x-0 bottom-10 grid grid-cols-1 grid-rows-2 gap-5">
+        <button class = "font-[JSans] text-[20px] bg-[#C5C5C5]  p-4 px-50 rounded-md shadow-xl">
+            Register a Child
+        </button>
+
+        <button class = "font-[JSans] text-[20px] bg-[#C5C5C5]  p-4 px-50 rounded-md shadow-xl">
+           Staff Login
+        </button>
+   </div>
+</body>
