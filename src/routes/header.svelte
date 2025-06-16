701346ca842720a7b0ce36c997eca5546ddb9029
Shoufou <riverap1231@gmail.com>
2025-06-16 22:30:56 +0800

    added login page

---
+++ src/routes/header.svelte
@@ -0,0 +1,15 @@
+
+<script>
+    let logoimg = '/img/logo.png';
+    let profileimg = '/img/Profile Icon.png'
+</script>
+
+<div id = "navbar">
+    <img src = {logoimg} alt = "logo" id = "logo">
+    <p> Home</p>
+    <p> Members</p>
+    <p> Events</p>
+    <p id = "reports"> Reports </p>
+
+    <img src = {profileimg} alt = "profile" id = "profile"/>
+</div>
