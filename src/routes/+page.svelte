216547da168b7b67e5477ffe4b9108774e92ca1f
Shoufou <riverap1231@gmail.com>
2025-06-17 21:21:37 +0800

    fixed box shadow on hover for login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,11 @@
    class="hero min-h-screen"
    style = "background-image: url(/img/bgimage.png);">
   <div class = "!mx-55 absolute inset-x-0 bottom-10 grid grid-cols-1 grid-rows-2 gap-5">
-        <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md">
+        <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md hover:!shadow-xl">
             Register a Child
         </button>
 
-      <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md">
+      <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md hover:!shadow-xl">
            Staff Login
         </button>
    </div>
