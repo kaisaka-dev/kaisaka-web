6a5c278a4006055a73185d6c7b1e3decac23ae96
Shoufou <riverap1231@gmail.com>
2025-06-18 01:03:05 +0800

    added modal to login page and components folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+   import LoginModal from '../components/LoginModal.svelte'
+   //var controls if the modal is on screen
+   let showModal = $state(false)
+
+   //function that closes the modal
+   const handleClose = () => {
+      showModal = false;
+   }
+</script>
+
 <div
    class="hero min-h-screen"
    style = "background-image: url(/img/bgimage.png);">
@@ -6,12 +17,27 @@
             Register a Child
         </button>
 
-      <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md hover:!shadow-xl">
+      <button 
+      onclick = {() => (showModal = true)}
+      class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md hover:!shadow-xl">
            Staff Login
         </button>
    </div>
 
+
 </div>
+
+<LoginModal bind:showModal>
+	{#snippet header()}
+		<img src = '/img/logo.png' alt = 'logo'
+      class = "!mx-85 w-55 h-40"/>
+	{/snippet}
+
+   
+   <p class = "!text-[var(--green)] !mb-11"> USERNAME</p>
+   <p class = "!text-[var(--green)]"> PASSWORD</p>
+
+</LoginModal>
 
 
 
