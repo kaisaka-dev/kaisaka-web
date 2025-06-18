f3fd16cefc33e476c6dd10eebda44e2542897da0
Roan Cedric Campo <roan_campo@dlsu.edu.ph>
2025-06-18 19:43:57 +0800

    Barangay Model + API documentation

--- src/lib/models/barangaysModel.ts
+++ src/lib/models/barangaysModel.ts
@@ -3,25 +3,77 @@ import TableManager, { type tableRow } from '../types/manager.js';
 
 type BarangayRow = tableRow<"barangays">
 
+/**
+ * A model concerning about barangay API. 
+ * 
+ * Refer to `src/lib/models/db.md` about what is exactly is a model.
+ */
 export class BarangayModel extends TableManager<"barangays">('barangays') {
   public static instance: BarangayModel = new BarangayModel();
 
+  /**
+   * Finds a barangay by name
+   * @param name name of Barangay
+   * @returns 
+   */
   async findByName(name: string): Promise<BarangayRow[] | null> {
     return this.findMany({ name })
   }
 
+  /**
+   * Finds a barangay by city
+   * @param city name of City
+   * @returns 
+   */
   async findByCity(city: string): Promise<BarangayRow[] | null> {
     return this.findMany({ city })
   }
 
+  /**
+   * Finds a barangay by its barangay number
+   * @param num number of barangay
+   * @returns 
+   */
   async findByNum(num: string): Promise<BarangayRow[] | null> {
     return this.findMany({ num })
   }
 
-  async insertBarangay(id: number, name: string, city: string, num: string) {
-    const barangay: Partial<BarangayRow> = {id, name, city, num}
-    let data = await this.insertOne(barangay);
-    console.log(data)
+  /**
+   * Insert a barangay to supabase
+   * @param id - the id number
+   * @param name - the name of the barangay
+   * @param city - the name of city its in
+   * @param num - optional, the barangay number
+   * @returns none
+   */
+  async insertBarangay(name: string, city: string, num: string) {
+    const barangay: Partial<BarangayRow> = {name, city, num}
+    const data = await this.insertOne(barangay);
+      
     return data 
+  }
+
+  async updateName(brgy_id: number, name: string){
+    const reference: Partial<BarangayRow> = { id: brgy_id }
+    const updates: Partial<BarangayRow> = { name: name }
+    const data = await this.updateOne(reference, updates)
+
+    return data
+  }
+
+  async updateCity(brgy_id: number, city_id: string){
+    const reference: Partial<BarangayRow> = { id: brgy_id }
+    const updates: Partial<BarangayRow> = { city: city_id }
+    const data = await this.updateOne(reference, updates)
+
+    return data
+  }
+
+  async updateNum(brgy_id: number, num: string){
+    const reference: Partial<BarangayRow> = { id: brgy_id }
+    const updates: Partial<BarangayRow> = { num: num }
+    const data = await this.updateOne(reference, updates)
+
+    return data
   }
 }
--- src/lib/models/db.md
+++ src/lib/models/db.md
@@ -1,3 +1,42 @@
 # Database
 
 This is where our database managers would be.
+
+3 tasks that we can easily separate else we make it very redundant for us
+
+- better dev experience, so we use this pattern
+
+## Database repository
+
+- will interact with the supabase backend, all logic is handled by this repository (this file)
+
+## Concrete repositories
+
+- actual implementation of this (DB repository) abstract notion of a repository
+
+## API logic / controller interface
+
+- any communications with the client will be handled here
+- ex: client needs to know certain barangays in the neighborhood
+- ex: if they have invalid data, it'll send it in a message / 400 bad request type thing
+
+## Example: Barangay pipeline
+
+- deals with everything barangay information
+- database logic / table manager: anything with supabase, it'll handle it
+  - delicate, refrain from touching unless necessary
+- business logic / barangay model: a class that can communicate with the table manager
+  - barangay model is handled by the barangay api, it will handle all the requests related to the barangay object logic
+  - if you want to find a certain name / city / etc.
+- controller logic / barangay api: the code will just validate anything from the client
+
+note to developers: roan already made it work, so if you want you can just replicate the existing barangay stuff (the thought pattern, so dont need to think too much)
+
+- business logic: it creates crud operations (every single model should handle every single crud operation right here)
+- if you're developing: prioritize the model first, then do API after
+
+note to QA: based on the model, you can create unit tests / future unit tests ahead of time
+
+note to designers: for the api, if you need to retrieve any sort of information, ex: login page, if you want to search children, go to children api and search for whatever necessary headers / body you need to pass.
+
+note to @ImaginaryLogs: session api soon
