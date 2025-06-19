166119acae2025684521343beabf010996d0ab7b
mika <mikaelaamon@gmail.com>
2025-06-19 22:07:50 +0800

    Create employmentStatusModel.ts

---
+++ src/lib/models/employmentStatusModel.ts
@@ -0,0 +1,42 @@
+import TableManager, { type tableRow } from '../types/manager.js';
+import type { Database } from '../types/supabase-types.ts';
+
+type employment_type_enum = Database['public']['Enums']['employment_type_enum'];
+type EmploymentStatusRow = tableRow<"employment_status">
+
+export class EmploymentStatusModel extends TableManager<"employment_status">('employment_status') {
+  public static instance: EmploymentStatusModel = new EmploymentStatusModel();
+  async insertEmploymentStatus(able_to_work: boolean, employment_type: employment_type_enum | null, member_id: string){
+    const finalEmploymentType = able_to_work ? employment_type : null; //prevents having employment type if able_to_work is false
+    
+    const employment_status : Partial<EmploymentStatusRow> = { able_to_work: able_to_work, employment_type: finalEmploymentType, member_id: member_id }
+    const data = await this.insertOne(employment_status)
+
+    return data
+  }
+
+  async findById(id: number){
+    return this.findOne({ id: id })
+  }
+
+  async findByMemberId(member_id: string){
+    return this.findOne({ member_id: member_id })
+  }
+
+  async updateEmploymentStatus(id: number, able_to_work: boolean, employment_type: employment_type_enum | null){
+    const finalEmploymentType = able_to_work ? employment_type : null; //prevents having employment type if able_to_work is false
+    
+    const reference: Partial<EmploymentStatusRow> = { id: id }
+    const updates: Partial<EmploymentStatusRow> = { able_to_work : able_to_work, employment_type: finalEmploymentType}
+
+    const data = await this.updateOne(reference, updates)
+  }
+
+  async deleteById(id: number){
+    return this.deleteOne({ id: id }) !== null;
+  }
+
+  async deleteByMemberId(member_id: string){
+    return this.deleteOne({ member_id: member_id }) !== null;
+  }
+}
