e96252a0d205a4a60da63da398fb97333f7c8ccb
beansprout <150442539+bea-nsprout@users.noreply.github.com>
2025-06-19 11:22:20 +0800

    Added form components!

---
+++ src/components/input/Checkbox.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let label = "Checkbox";
+	export let id = "checkbox";
+</script>
+
+<style>
+  input[type='checkbox']:checked {
+      background-color: var(--green);
+  }
+</style>
+
+<div class="input-container" style="align-items: center; gap:8px">
+	<input type="checkbox" id="{id}" name="{id}" value="Bike" class="check" style=" height:10px; width: 10px; padding: 10px;">
+	<label for={id}>{label}</label>
+
+
+</div>
