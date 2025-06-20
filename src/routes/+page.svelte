7b9792080742cdb4a18097c24a8dbae97acb6d10
bea-nsprout <beaantoinette26@gmail.com>
2025-06-20 13:10:33 +0800

    Added child registration page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,8 @@
    class="hero min-h-screen"
    style = "background-image: url(/img/bgimage.png);">
   <div class = "!mx-55 absolute inset-x-0 bottom-10 grid grid-cols-1 grid-rows-2 gap-5">
-        <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md hover:!shadow-xl">
+        <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md hover:!shadow-xl"
+                onclick={() => location.href = '/registration/child'}>
             Register a Child
         </button>
 
