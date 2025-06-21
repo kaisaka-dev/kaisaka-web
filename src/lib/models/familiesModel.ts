b0eb3f25fdb4d3d5b7ebe0cde090f39df3086a98
mika <mikaelaamon@gmail.com>
2025-06-21 22:02:15 +0800

    Create familiesModel.ts

---
+++ src/lib/models/familiesModel.ts
@@ -0,0 +1,31 @@
+import TableManager, { type tableRow } from '../types/manager.js';
+
+
+type FamiliesRow = tableRow<"families">
+
+/**
+ * A model concerning about CRUD operations on *families information*. 
+ * 
+ * Made for the `cities` API. 
+ * 
+ * **Reference**: Database Model `src/lib/models/db.md`
+ */
+export class FamiliesModel extends TableManager<"families">('families') {
+  public static instance: FamiliesModel = new FamiliesModel();
+    async createFamily(){
+        const now = new Date().toISOString()
+        const data = await this.insertOne({ date_created: now })
+    }
+
+    async findById(id: string){
+        return this.findOne({ id: id })
+    }
+
+    async getAll(filter: Partial<FamiliesRow> = {}){
+        return this.findMany(filter)
+    }
+
+    async deleteById(id: string){
+        return this.deleteOne({ id: id }) !== null;
+    }
+}
