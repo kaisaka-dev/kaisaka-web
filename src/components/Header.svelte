e9a3a5922f3e08cec8dc3797742af4979b0019e1
bea-nsprout <beaantoinette26@gmail.com>
2025-06-24 00:55:41 +0800

    Added: Dropdown hover (no link yet)

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,12 +6,44 @@
 
 <div id = "navbar" style="font-weight:bold">
     <img src = {logoimg} alt = "logo" id = "logo">
-    <a href="/" > Home</a>
-    <a href="/"> Members</a>
-    <a href="/"> Events</a>
-    <a id = "reports" href="/"> Reports </a>
+    <a href="/" id="home"> Home</a>
 
-    <img src = {profileimg} alt = "profile" id = "profile"/>
+    <div class="dropdown dropdown-hover">
+        <div class="navlink" role="button" tabindex="-1">Members</div>
+        <ul tabindex="-1" class="dropdown-content menu rounded-box z-1 w-52 p-2 shadow-sm !bg-[#513737]">
+            <li><a>Children</a></li>
+            <li><a>Caregivers</a></li>
+            <li><a>Families</a></li>
+            <li><a>Pending</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown dropdown-hover">
+        <div class="navlink" role="button" tabindex="-1">Events</div>
+        <ul tabindex="-1" class="dropdown-content menu rounded-box z-1 w-52 p-2 shadow-sm !bg-[#513737]">
+            <li><a>Target Events</a></li>
+            <li><a>Conducted Events</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown dropdown-hover">
+        <div class="navlink" role="button" tabindex="-1">Reports</div>
+        <ul tabindex="-1" class="dropdown-content menu rounded-box z-1 w-52 p-2 shadow-sm !bg-[#513737]">
+            <li><a>Item</a></li>
+            <li><a>Item</a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown dropdown-left dropdown-hover" id="profile">
+        <div class="navlink" role="button" tabindex="-1"><img src = {profileimg} alt = "profile" /></div>
+
+        <ul tabindex="-1" class="dropdown-content menu rounded-box z-1 w-52 p-2 shadow-sm !bg-[#513737]">
+            <li><a>My Profile</a></li>
+            <li><a>Log Out</a></li>
+        </ul>
+    </div>
+
+
 </div>
 
 <style>
@@ -20,15 +52,21 @@
         background-color : #513737;
         display:flex;
         padding: 10px;
+        height: 90px;
     }
 
-    #navbar > a {
-        color: #F7B0CD;
+    a, .navlink {
+        color: var(--pink);
         padding: 15px;
         font-size: var(--medium-text);
     }
-    #navbar > a:hover {
+    a:hover, .navlink:hover {
         color: var(--green);
+    }
+
+    li > a {
+        font-size: var(--small-text);
+        font-weight: 500;
     }
 
     #navbar > #logo {
@@ -38,7 +76,6 @@
 
     #navbar > #profile {
         height: 70px;
-        padding-top:20px;
         margin-left: auto;
     }
 
