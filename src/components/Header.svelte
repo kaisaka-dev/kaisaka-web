a023c3403a2de7dedc5ab3fcc5b4f07991e085b1
bea-nsprout <beaantoinette26@gmail.com>
2025-06-24 02:17:55 +0800

    Added: Subnav (no links yet)

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,9 @@
 
 <script>
     let logoimg = '/img/logo.png';
-    let profileimg = '/img/Profile Icon.png'
+    let profileimg = '/img/Profile Icon.png';
+    export let curr_category = "members";
+    export let curr_page = "children";
 </script>
 
 <div id = "navbar" style="font-weight:bold">
@@ -46,13 +48,34 @@
 
 </div>
 
+<div id="subnav">
+    {#if curr_category === "members"}
+        <ul>
+            <li><a>Children</a></li>
+            <li><a>Caregivers</a></li>
+            <li><a>Families</a></li>
+            <li><a>Pending</a></li>
+        </ul>
+    {/if}
+
+    {#if curr_category === "events"}
+        <ul>
+            <li><a>Target Events</a></li>
+            <li><a>Conducted Events</a></li>
+        </ul>
+    {/if}
+</div>
+
 <style>
 
     #navbar {
+        position: relative;
         background-color : #513737;
         display:flex;
         padding: 10px;
         height: 90px;
+        box-shadow: 0 4px 4px rgba(0, 0, 0, 0.4);
+        z-index: 2;
     }
 
     a, .navlink {
@@ -78,5 +101,46 @@
         height: 70px;
         margin-left: auto;
     }
+
+    #subnav {
+        position: relative;
+        background-color: var(--pink);
+        height: auto;
+        z-index: 1;
+    }
+
+
+    #subnav li:hover {
+        background-color: rgb(123, 88, 102);
+        transition: 0.3s;
+    }
+
+    #subnav ul {
+        display: flex;
+        flex-direction: row;
+        margin: 0;
+        list-style: none;
+    }
+
+    #subnav li {
+        display: flex;
+        align-items: center;
+        margin: 0;
+        padding: 0 2rem;
+        height: 100%;
+    }
+
+    #subnav a {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        padding: 0 10px;
+        color: var(--background);
+        font-weight: bold;
+        font-size: var(--small-text);
+        text-decoration: none;
+    }
+
+
 
 </style>
