3f2b9ea9de23fefe5de3e0b8ef9b6d60c008b0ee
bea-nsprout <beaantoinette26@gmail.com>
2025-07-01 00:15:20 +0800

    Update: centered button formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,14 +13,14 @@
 <div
    class="hero min-h-screen"
    style = "background-image: url(/img/bgimage.png);">
-  <div class = "!mx-55 absolute inset-x-0 bottom-10 grid grid-cols-1 grid-rows-2 gap-5">
-        <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md hover:!shadow-xl">
+  <div class = "absolute inset-x-0 bottom-10 grid grid-cols-1 grid-rows-2 gap-5 sm:mx-auto" style="margin-inline: clamp(1rem, 10vw, 220px)" >
+        <button class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8  !rounded-md hover:!shadow-xl">
             Register a Child
         </button>
 
       <button 
       onclick = {() => (showModal = true)}
-      class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8 !px-50 !rounded-md hover:!shadow-xl">
+      class = "btn !text-black font-[JSans] !text-[var(--medium-text)] !bg-[#C5C5C5]  !p-8  !rounded-md hover:!shadow-xl">
            Staff Login
         </button>
    </div>
