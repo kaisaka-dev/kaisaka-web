b513f4fc791216839b0d06a02e4d382f5d026054
bea-nsprout <beaantoinette26@gmail.com>
2025-07-03 00:47:20 +0800

    Added: year type

--- src/components/input/InputDate.svelte
+++ src/components/input/InputDate.svelte
@@ -1,18 +1,19 @@
-<script>
+<script lang="ts">
 	import Validation from '../text/Validation.svelte';
 
 	export let label = "Date";
 	export let id = "date";
 	export let required = false;
 	export let msg = "";
-	export let value = "";
+	export let value: string | number = "";		// string for date, number for year
+	export let type = "date"; // for the type of date input
 </script>
 
 <div class="input-container">
 	<label for={id}>{label} {required ? " *" : ""}</label>
 
 	<div class="input-wrapper">
-		<input type="date" id={id} class="input" bind:value={value} />
+		<input type={type} id={id} class="input" bind:value={value} />
 
 		{#if msg}
 			<Validation msg={msg} />
