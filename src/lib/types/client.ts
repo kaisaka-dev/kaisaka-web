f61a5c6a9c6f458411f9c831f4d4d18a7b9a71b8
iofas <jphhelbling03@gmail.com>
2025-07-14 00:11:31 +0800

    Changed client.ts to process local env

--- src/lib/types/client.ts
+++ src/lib/types/client.ts
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+// comment out below 2 lines when running remotely
+import dotenv from 'dotenv';
+dotenv.config({path: '.env.local'});
+
 export const supabase = createClient(
   process.env.SUPABASE_URL ?? '',
   process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
