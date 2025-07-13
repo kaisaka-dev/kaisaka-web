78d69bd85120cdc78f1d6b8a55a0d50140621353
Roan Cedric Campo <roan_campo@dlsu.edu.ph>
2025-07-14 04:05:32 +0800

    fixed login

--- src/lib/server/auth-actions.ts
+++ src/lib/server/auth-actions.ts
@@ -1,35 +1,31 @@
-import { fail, redirect, type Action } from "@sveltejs/kit"
-
+import { redirect, type Action } from "@sveltejs/kit"
+import { error } from "@sveltejs/kit"
 
 export const login: Action = async ({ request, locals: { supabase } }) => {
-    console.log('[Server] Login Hit')
+  console.log('[Server] Login Hit')
+  
+  const formData = await request.formData()
+  const email = formData.get('email') as string
+  const password = formData.get('password') as string
+  console.log('[Server] Looking for: ', email)
 
-    
-    const formData = await request.formData()
-    const email = formData.get('email') as string
-    const password = formData.get('password') as string
-    console.log('[Server] Looking for: ', email)
+  
+  if (!email) return redirect(404, `/?error=${encodeURIComponent('Email is required')}`)
+  
+  if (!password) return redirect(404, `/?error=${encodeURIComponent('Password is required')}`)
+  
+  console.log('Signing...')
+  
+  const { data, error: authError } = await supabase.auth.signInWithPassword({ email: email, password: password })
+  
+  console.log('Supabase', { data, error })
 
-
-    if (!email) return fail(400, { error: 'Email is required', email: email })
-    
-    if (!password) return fail(400, { error: 'Password is required', password: password })
-    
-    console.log('Signing...')
-    
-    const { data, error } = await supabase.auth.signInWithPassword({ email: email, password: password })
-    
-    console.log('Supabase', { data, error })
-
-    if (error) {
-      console.error(error)
-      return fail(400, { error: error.message })
-    } else {
-      
-      console.log('✅ Login successful:', data.session ? 'Session created' : 'No session')
-
-
-      redirect(303, '/dashboard')
-      return { success: true }
-    }
-  } 
+  if (authError) {
+    console.error(authError)
+    return redirect(404, `/?error=${encodeURIComponent(`${authError.name} ${authError.message} ${authError.cause}`)}`)
+  } else {
+    console.log('✅ Login successful:', data.session ? 'Session created' : 'No session')
+  
+    redirect(303, '/dashboard')
+  }
+} 
