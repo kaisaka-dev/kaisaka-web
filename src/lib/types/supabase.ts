6b53f887c137ba26a8a820600cc11c9db733f351
iofas <jphhelbling03@gmail.com>
2025-07-18 18:00:30 +0800

    Adjusted supabase.ts to start client locally

--- src/lib/types/supabase.ts
+++ src/lib/types/supabase.ts
@@ -1,13 +1,18 @@
-import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+// comment out below 2 lines when running remotely
+import dotenv from 'dotenv';
+dotenv.config({path: '.env.local', override: true});
 
-export const supabaseURL = PUBLIC_SUPABASE_URL
-export const supabaseAnonKey = PUBLIC_SUPABASE_ANON_KEY
+import { createClient } from '@supabase/supabase-js';
+import { env } from '$env/dynamic/public';
+
+export const supabaseURL = env.PUBLIC_SUPABASE_URL
+export const supabaseAnonKey = env.PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
   supabaseURL,
   process.env.SUPABASE_SERVICE_ROLE_KEY ?? '' 
 );
 
-
-
+//console.log('Supabase URL: ', supabaseURL);
+//console.log('Supabase Anon Key: ', supabaseAnonKey);
+//console.log('Supabase Service Role Key: ', process.env.SUPABASE_SERVICE_ROLE_KEY);
