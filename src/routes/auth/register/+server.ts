51c32b8dc989414b42db54116c896a685e5673c4
mika <mikaelaamon@gmail.com>
2025-07-20 19:16:36 +0800

    bug fixes

--- src/routes/auth/register/+server.ts
+++ src/routes/auth/register/+server.ts
@@ -1,6 +1,10 @@
 import { supabase } from '$lib/types/supabase.js';
 import { json } from '@sveltejs/kit';
 
+function isValidEmail(email: string): boolean {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
 
 export async function POST({ request }) {
   console.log('[Server] Login Hit')
@@ -14,6 +18,34 @@ export async function POST({ request }) {
       const err_missing_requirements = 'User, Email, and Password is required'
       return json({ error: err_missing_requirements}, { status: 400, statusText: err_missing_requirements} )
     }
+
+    const { data: existingUser } = await supabase.from('user_profiles').select('*').eq('username', username);
+    console.log(existingUser)
+
+    if (existingUser.length !== 0) {
+      return json(
+        { error: 'Username is already taken.' },
+        { status: 400, statusText: 'Username is already taken.' }
+      );
+    }
+
+    if (!isValidEmail(email)) {
+      return json(
+        { error: 'Email format is invalid.' },
+        { status: 400 }
+      );
+    }
+
+    const {data: existingEmail } = await supabase.from('user_profiles').select('*').eq('email',email);
+    console.log(existingEmail)
+
+    if (existingEmail.length !== 0) {
+      return json(
+        { error: 'Email is already taken.' },
+        { status: 400, statusText: 'Email is already taken.' }
+      );
+    }
+
     const { data, error } = await supabase.auth.admin.createUser({
       email,
       password,
