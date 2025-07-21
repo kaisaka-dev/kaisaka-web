27cc20a85845b1a4884de0b95030890bfe677ab6
mika <mikaelaamon@gmail.com>
2025-07-21 16:21:27 +0800

    fixed nav bar not logging out

--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -1,7 +1,12 @@
-import { redirect } from '@sveltejs/kit'
+import { json, redirect } from '@sveltejs/kit'
 
 export async function POST({ locals: { supabase } }) {
-  await supabase.auth.signOut()
-  console.log("[Server] Logout")
-  throw redirect(302, '/')
+  const { error } = await supabase.auth.signOut();
+  console.log("[Server] Logout");
+
+  if (error) {
+    return new Response('Failed to logout', { status: 500 });
+  }
+
+  return json({ success: true });
 }
