ea359d200b508a92113b972e63c96e4b945f6567
mika <mikaelaamon@gmail.com>
2025-07-22 04:32:45 +0800

    added update staff-profile stuff

---
+++ src/routes/auth/update/+server.ts
@@ -0,0 +1,83 @@
+import { supabase } from '$lib/types/supabase.js';
+import { json, redirect } from '@sveltejs/kit';
+
+function isValidEmail(email: string): boolean {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+export async function POST({ request, locals }) {
+  console.log('[Server] Login Hit')
+  if(!locals.user){
+    throw redirect(302, '/')
+  }
+
+  try {
+    const body = await request.json();
+    const current_profile = locals.profile
+
+    const {username, email, password} = body
+    console.log(current_profile)
+    // Create user in Supabase Auth
+
+    if (!username && !email && !password) {
+      const err_missing_requirements = 'User, Email, or Password is required'
+      return json({ error: err_missing_requirements}, { status: 400, statusText: err_missing_requirements} )
+    }
+
+    if(username !== current_profile.accountName){
+        const { data: existingUser } = await supabase.from('user_profiles').select('*').eq('username', username);
+
+        if (existingUser.length !== 0) {
+            return json(
+                { error: 'Username is already taken.' },
+                { status: 400, statusText: 'Username is already taken.' }
+            );
+        }
+    }
+
+    if(email !== current_profile.email){
+        if (!isValidEmail(email)) {
+        return json(
+            { error: 'Email format is invalid.' },
+            { status: 400, statusText: 'Email format is invalid.' }
+        );
+        }
+
+        const {data: existingEmail } = await supabase.from('user_profiles').select('*').eq('email',email);
+        console.log(existingEmail)
+
+        if (existingEmail.length !== 0) {
+        return json(
+            { error: 'Email is already taken.' },
+            { status: 400, statusText: 'Email is already taken.' }
+        );
+        }
+    }
+
+    const { data, error } = await supabase.auth.admin.updateUserById( current_profile.id, { 
+      email: email,
+      password: password,
+      data: {
+        username: username
+      }
+    });
+
+    console.log('[Server] Auth Update Result:', data, error?.message)
+
+    if (error) {
+      return json(
+        { error: error.message },
+        { status: 400, statusText: error.message }
+      );
+    }
+
+    return json({ success: true });
+
+  } catch (error) {
+    console.error('User Update error:', error);
+    return json(
+      { message: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
--- src/routes/dashboard/staff-profile/edit/+page.svelte
+++ src/routes/dashboard/staff-profile/edit/+page.svelte
@@ -7,28 +7,24 @@
 
     export let data;
 
-    //below are test data needed for the page to work, this will be deleted once the relevant APIs are completed
     let user:staff = {
         accountName: data.accountName,
-        role: data.role,
-        password: data.password,
         email: data.email
     }
 
-    let confirmpass = user.password
-    let testpass = user.password
+    let confirmpass = ""
+    let testpass = ""
     let testacc = user.accountName
     let testemail = user.email
-    let testrole = user.role
     
 
-    let passwordmismatch =false;
+    let passwordmismatch = false;
     let missinginfo = false;
     let noissues = false;
 
     let style = ""
     let state = ""
-    function handleSubmit(account: string, pass: string,confirmpass: string, email: string) {
+    async function handleSubmit(account: string, pass: string,confirmpass: string, email: string) {
         if(account === "" || pass === "" || confirmpass === "" || email === "") {
            style = "mt-5 ml-25 !text-red-500"
            state = "Missing Information!"
@@ -40,8 +36,26 @@
          }
 
         else {
-            style = "mt-5 ml-25 !text-[var(--green)]"
-           state = "Changes saved!"       
+            const updateRes = await fetch('/auth/update', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json' 
+                },
+                body: JSON.stringify({
+                    username: testacc,
+                    password: testpass,
+                    email: testemail
+                })
+                });
+
+            if(!updateRes.ok){
+                style = "mt-5 ml-25 !text-red-500"
+                state = updateRes.statusText
+            }
+            else{
+                style = "mt-5 ml-25 !text-[var(--green)]"
+                state = "Changes saved!"
+            }
         }
     }
 </script>
@@ -56,7 +70,6 @@
     <h1> {user.accountName}'s Profile</h1>
 </section>
 
-<form method = "GET">
 <!-- container for profile content-->
 <div class = "border-4 border-[var(--border)] w-300 min-w-150 ml-25 p-6">
     <div class = "flex flex-col">
@@ -71,16 +84,9 @@
         </div>
 
         <div class = "information mt-5">
-            <div class = "w-50"> Confirm Account Password </div>
+            <div class = "w-50"> Confirm Password </div>
             <div class = "ml-3"> <input class = "w-80 rounded-md" type = "password" bind:value = {confirmpass}/> </div>
         </div>
-
-        {#if user.role === "superadmin"} <!--this is something that will render only if youre a superadmin because i think its wonky if a regular staff member can change their role lol-->
-        <div class = "information mt-5">
-            <div class = "w-50"> Account Role: </div>
-            <div class = "-ml-57"> <Select  label = "" bind:value = {testrole} options = {["superadmin","admin"]}/> </div>
-        </div>
-        {/if}
 
         <div class = "information mt-5">
             <div class = "w-50"> Account Email: </div>
@@ -98,4 +104,3 @@
 <div class = {style}>
    {state}
 </div>
-</form>
