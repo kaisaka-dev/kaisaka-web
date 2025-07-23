7b56d565718bf96f6c7944094872e09a10597df6
bea-nsprout <beaantoinette26@gmail.com>
2025-07-23 16:30:29 +0800

    Update: more warning messages

--- src/routes/dashboard/reports/list/ReportPeriodModal.svelte
+++ src/routes/dashboard/reports/list/ReportPeriodModal.svelte
@@ -4,7 +4,6 @@ import InputRange from '$components/input/InputRange.svelte';
 import Modal from '$components/Modal.svelte';
 import Textarea from '$components/input/InputTextarea.svelte';
 import type { ReportPeriod, Errors } from './+page.server.js';
-import { enhance } from '$app/forms';
 
 export let modalIsOpen = false;
 export let title = "";
@@ -39,6 +38,9 @@ let errors: Errors = {
 	new_actual_CWDS: "",
 	old_actual_CWDS: ""
 }
+
+const thisYear = new Date().getFullYear();  // used to verify if year makes sense
+
 /**
  * validates the form before submission
  */
@@ -51,13 +53,14 @@ function validateForm(): boolean {
 		endYYYY: formData.endYYYY ? "" : "End Year is required",
 		endMM: errors.endMM,
 		endDD: errors.endDD,
-		new_target_CWDS: formData.new_target_CWDS && formData.new_target_CWDS < 0 || formData.new_target_CWDS === null
+		// for the database, int2's range is only -32768 to 32767
+		new_target_CWDS: formData.new_target_CWDS && (formData.new_target_CWDS < 0 || formData.new_target_CWDS > 32767) || formData.new_target_CWDS === null
 			? "Invalid" : "",
-		old_target_CWDS: formData.old_target_CWDS && formData.old_target_CWDS < 0 || formData.old_target_CWDS === null
+		old_target_CWDS: formData.old_target_CWDS && (formData.old_target_CWDS < 0 || formData.old_target_CWDS > 32767) || formData.old_target_CWDS === null
 			? "Invalid" : "",
-		new_actual_CWDS: formData.new_actual_CWDS && formData.new_actual_CWDS < 0 || formData.new_actual_CWDS === null
+		new_actual_CWDS: formData.new_actual_CWDS && (formData.new_actual_CWDS < 0 || formData.new_actual_CWDS > 32767)
 			? "Invalid" : "",
-		old_actual_CWDS: formData.old_actual_CWDS && formData.old_actual_CWDS < 0 || formData.old_actual_CWDS === null
+		old_actual_CWDS: formData.old_actual_CWDS && (formData.old_actual_CWDS < 0 || formData.old_actual_CWDS > 32767)
 			? "Invalid" : ""
 	}
 	if (formData.endYYYY !== null && formData.startYYYY !== null) {
@@ -90,30 +93,27 @@ async function handleSubmit() {
 		const method = formData.id ? 'PUT' : 'POST';
 		const url = formData.id ? `/api/annual_program` : `/api/annual_program`;
 
-		console.log(method)
-		const submission = JSON.stringify({
-			id: formData.id,
-			start_year: formData.startYYYY,
-			start_month: formData.startMM,
-			start_date: formData.startDD,
-			end_year: formData.endYYYY,
-			end_month: formData.endMM,
-			end_date: formData.endDD,
-			target_new_cwds: formData.new_target_CWDS,
-			target_old_cwds: formData.old_target_CWDS,
-			actual_new_cwds: formData.new_actual_CWDS,
-			actual_old_cwds: formData.old_actual_CWDS,
-			general_reflection: formData.general_reflection,
-			lessons_learned: formData.lessons_learned
-		})
-		console.log(submission)
 
 		const response = await fetch(url, {
 			method,
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: submission
+			body: JSON.stringify({
+				id: formData.id,
+				start_year: formData.startYYYY,
+				start_month: formData.startMM,
+				start_date: formData.startDD,
+				end_year: formData.endYYYY,
+				end_month: formData.endMM,
+				end_date: formData.endDD,
+				target_new_cwds: formData.new_target_CWDS,
+				target_old_cwds: formData.old_target_CWDS,
+				actual_new_cwds: formData.new_actual_CWDS,
+				actual_old_cwds: formData.old_actual_CWDS,
+				general_reflection: formData.general_reflection,
+				lessons_learned: formData.lessons_learned
+			})
 		});
 
 		if (!response.ok) {
@@ -121,12 +121,19 @@ async function handleSubmit() {
 		}
 
 		modalIsOpen = false;
+		if (method === 'POST') window.location.reload();	// refresh if post, to reload the data.
 	} catch (error) {
 		console.error('Submission error:', error);
 	}
 }
 
 // error checks
+$: if (formData.startYYYY == null) errors.startYYYY = ""
+	else if (formData.startYYYY >= thisYear-100 && formData.startYYYY <= thisYear+100) errors.startYYYY = ""
+	else errors.startYYYY = "warning: year too far away"
+$: if (formData.endYYYY == null) errors.endYYYY = ""
+	else if (formData.endYYYY >= thisYear-100 && formData.endYYYY <= thisYear+100) errors.endYYYY = ""
+	else errors.endYYYY = "warning: year too far away"
 $: if (formData.startMM == null) errors.startMM = ""
 	else if (formData.startMM >= 1 && formData.startMM <= 12) errors.startMM = ""
 	else errors.startMM = "month: 1 to 12 only"
@@ -157,7 +164,7 @@ $: if (formData.endDD == null) errors.endDD = ""
 			<Textarea label="General Reflection" id="reflection" bind:value={formData.general_reflection} />
 			<Textarea label="Lessons Learned" id="lessons" bind:value={formData.lessons_learned} />
 
-			<button on:click={() => modalIsOpen = false}>Cancel</button>
+			<button type="button" on:click={() => modalIsOpen = false}>Cancel</button>
 			<button class="green" type="submit">Submit</button>
 		</form>
 	</div>
