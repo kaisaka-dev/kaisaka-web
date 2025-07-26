cf08549c89b06d417e5acc2bf90e39789e90babb
mika <mikaelaamon@gmail.com>
2025-07-27 01:45:31 +0800

    Update +page.server.ts

--- src/routes/registration/child/+page.server.ts
+++ src/routes/registration/child/+page.server.ts
@@ -1,4 +1,4 @@
-import type { PageLoad } from '../../../../../.svelte-kit/types/src/routes/$types.js';
+import type { PageLoad } from '../../../../.svelte-kit/types/src/routes/$types.js';
 
 export const load: PageLoad = async({fetch}) => {
 
