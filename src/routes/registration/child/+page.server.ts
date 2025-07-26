743eaca792bdc87493b3f68dfa8f814c31c9db20
mika <mikaelaamon@gmail.com>
2025-07-27 01:50:12 +0800

    Update +page.server.ts

---
+++ src/routes/registration/child/+page.server.ts
@@ -0,0 +1,15 @@
+import type { PageServerLoad } from '../../../../.svelte-kit/types/src/routes/$types.js';
+
+export const load: PageServerLoad = async({fetch}) => {
+
+	// fetching the (dropdown) options for disability category
+	const response = await fetch('/api/disability_category');
+	const options_disCategory = await response.json();
+
+	return {
+		options: {
+			disCategory: options_disCategory.data
+
+		}
+	};
+}
