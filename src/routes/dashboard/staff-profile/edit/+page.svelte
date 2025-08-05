0701ae3b47661b15e31df764a26f9749de590485
bea-nsprout <beaantoinette26@gmail.com>
2025-08-05 20:54:53 +0800

    Added: password validation, show password

--- src/routes/dashboard/staff-profile/edit/+page.svelte
+++ src/routes/dashboard/staff-profile/edit/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import Input from '$components/input/InputText.svelte'
     import Header from '$components/Header.svelte'
-    import Select from '$components/input/Select.svelte'
     import type { staff } from '$lib/types/staff.js'
-	import { userEvent } from '@storybook/test';
     import LoadingBtn from '$components/styled-buttons/LoadingBtn.svelte';
+    import { goto } from '$app/navigation';
+    import InputPassword from '$components/input/InputPassword.svelte';
 
     export let data;
 
@@ -13,42 +13,57 @@
         email: data.email
     }
 
-    let confirmpass = ""
-    let testpass = ""
-    let testacc = user.accountName
-    let testemail = user.email
-    
-
-    let passwordmismatch = false;
-    let missinginfo = false;
-    let noissues = false;
-
     let style = ""
     let state = ""
-    let loadingsave = false
+    let loadingSave = false
 
-    async function handleSubmit(account: string, pass: string,confirmpass: string, email: string) {
-        if(account === "" || pass === "" || confirmpass === "" || email === "") {
+    let account = {
+        name: user.accountName,
+        email: user.email,
+        pass: '',
+        confirmPass: ''
+    }
+    let errors = {
+        pass: '',
+        confirmPass: ''
+    }
+
+    function isStrongPassword(password: string): boolean {
+        const hasLetter = /[a-zA-Z]/.test(password);
+        const hasNumber = /[0-9]/.test(password);
+        const isLongEnough = password.length >= 8;
+
+        return hasLetter && hasNumber && isLongEnough;
+    }
+
+    async function handleSubmit() {
+        state = ""
+        if(account.name === "" || account.pass === "" || account.confirmPass === "" || account.email === "") {
            style = "mt-5 ml-25 !text-red-500"
            state = "Missing Information!"
         }
 
-        else if(pass !== confirmpass) {
+        else if(!isStrongPassword(account.pass)) {
             style = "mt-5 ml-25 !text-red-500"
-           state = "Password Mismatch!"       
+            state = "Password does not meet requirements!"
+        }
+
+        else if(account.pass !== account.confirmPass) {
+            style = "mt-5 ml-25 !text-red-500"
+            state = "Password Mismatch!"
          }
 
         else {
-            loadingsave = true;
+            loadingSave = true;
             const updateRes = await fetch('/auth/update', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json' 
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    username: testacc,
-                    password: testpass,
-                    email: testemail
+                    username: account.name,
+                    password: account.pass,
+                    email: account.email
                 })
                 });
 
@@ -59,53 +74,36 @@
             else{
                 style = "mt-5 ml-25 !text-[var(--green)]"
                 state = "Changes saved!"
+                goto("/dashboard/staff-profile")
             }
-            loadingsave = false;
+            loadingSave = false;
+
         }
     }
+
+    $: errors.pass = isStrongPassword(account.pass) ? "" : "At least 8 characters, and contains at least 1 letter and number"
 </script>
-<style>
-    .information{
-        display: flex;
-        flex-direction: row;
-    }
-</style>
- <Header/>
+
+<Header/>
+
 <section>
     <h1> {user.accountName}'s Profile</h1>
 </section>
 
 <!-- container for profile content-->
-<div class = "border-4 border-[var(--border)] w-300 min-w-150 ml-25 p-6">
-    <div class = "flex flex-col">
-        <div class = "information">
-            <div class = "w-50"> Account Name: </div>
-            <div class = "ml-3"> <Input  label = "" bind:value = {testacc}/> </div>
-        </div>
-
-        <div class = "information mt-5">
-            <div class = "w-50"> Account Password: </div>
-            <div class = "ml-3"> <input class = "w-80 rounded-md" type = "password" bind:value = {testpass}/> </div>
-        </div>
-
-        <div class = "information mt-5">
-            <div class = "w-50"> Confirm Password </div>
-            <div class = "ml-3"> <input class = "w-80 rounded-md" type = "password" bind:value = {confirmpass}/> </div>
-        </div>
-
-        <div class = "information mt-5">
-            <div class = "w-50"> Account Email: </div>
-            <div class = "ml-3"> <Input  label = "" bind:value = {testemail}/> </div>
-        </div>
-    </div>
-</div>
+<section class = "border-4 border-[var(--border)] min-w-150 p-6">
+    <Input id='name' label="Account name" bind:value={account.name}/>
+    <Input id='email' label="Account email" bind:value={account.email}/>
+    <InputPassword id='pass' label="Account password" bind:value={account.pass} msg={errors.pass}/>
+    <InputPassword id='confirmPass' label="Confirm password" bind:value={account.confirmPass} msg={errors.confirmPass}/>
+</section>
 
 <div class = "mt-5 ml-25 flex flex-row">
-    <button class =  "mr-5" type = "submit" onclick = {()=> location.href="/dashboard/staff-profile"}> Back</button>
-    {#if loadingsave}
+    <button class =  "mr-5" type = "submit" onclick = {()=> goto("/dashboard/staff-profile")}> Back</button>
+    {#if loadingSave}
         <LoadingBtn label="Save Changes" />
     {:else}
-        <button class = "green" onclick = {() => handleSubmit(testacc, testpass,confirmpass, testemail)}> Save Changes</button>
+        <button class = "green" onclick = {() => handleSubmit()}> Save Changes</button>
     {/if}
 
 </div>
