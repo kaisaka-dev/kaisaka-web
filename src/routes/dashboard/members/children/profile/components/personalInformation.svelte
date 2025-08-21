923fccd32116c57ac4c378456aed2f59a1a78d5c
bea-nsprout <beaantoinette26@gmail.com>
2025-08-22 00:39:48 +0800

    Update: margin adjustment

--- src/routes/dashboard/members/children/profile/components/personalInformation.svelte
+++ src/routes/dashboard/members/children/profile/components/personalInformation.svelte
@@ -54,25 +54,25 @@
     </h1>
 </div>
 <div class = "border-[var(--border)] border-4 ml-55 mr-10 !font-bold z-2000 max-w-290 mx-auto flex-wrap" >
-    <div class = "flex flex-col xl:flex-row !justify-start mt-2">
+    <div class = "flex flex-col xl:flex-row !justify-start p-4">
         <div class = "flex flex-col w-full max-w-280">
-                <div class = "mt-4 -ml-4"> <Input label = "First Name" disabled = {inputDisabled} required = {!disabled} msg = {errors.firstName} bind:value = {data.firstName} margin = {true}/> </div>
-                <div class = "-ml-4"> <Input label = "Middle Name" disabled = {inputDisabled} required = {false} bind:value = {data.middleName} margin = {true}/> </div>
-                <div class = "-ml-4"> <Input label = "Last Name" disabled = {inputDisabled} required = {!disabled} msg = {errors.lastName} bind:value = {data.lastName} margin = {true}/> </div>
-                <div class = "-ml-4"> <Input type = "date" label = "Birthday" required = {!disabled} msg = {errors.birthday} disabled = {inputDisabled} bind:value = {data.birthday} margin = {true}/> </div>
-                <div class = "-ml-4"> <Input label = "Age" disabled bind:value = {age} margin = {true}/> </div>
-                <div class = "-ml-4"> <Select label = "Sex" required = {!disabled} disabled = {inputDisabled} options = {dropdownOptions.sex}  bind:value = {data.sex} margin = {true}/> </div>
-                <div class = "-ml-4"> <Input label = "Address" required = {!disabled} msg = {errors.address} disabled = {inputDisabled} bind:value = {data.address} margin = {true}/> </div>
-                <div class = "-ml-4"> <Input label = "Barangay"  required = {!disabled} msg = {errors.barangay} disabled = {inputDisabled} bind:value = {data.barangay} margin = {true}/> </div>
+                <Input label = "First Name" disabled = {inputDisabled} required = {!disabled} msg = {errors.firstName} bind:value = {data.firstName} margin = {true}/>
+                <Input label = "Middle Name" disabled = {inputDisabled} required = {false} bind:value = {data.middleName} margin = {true}/>
+                <Input label = "Last Name" disabled = {inputDisabled} required = {!disabled} msg = {errors.lastName} bind:value = {data.lastName} margin = {true}/>
+                <Input type = "date" label = "Birthday" required = {!disabled} msg = {errors.birthday} disabled = {inputDisabled} bind:value = {data.birthday} margin = {true}/>
+                <Input label = "Age" disabled bind:value = {age} margin = {true}/>
+                <Select label = "Sex" required = {!disabled} disabled = {inputDisabled} options = {dropdownOptions.sex}  bind:value = {data.sex} margin = {true}/>
+                <Input label = "Address" required = {!disabled} msg = {errors.address} disabled = {inputDisabled} bind:value = {data.address} margin = {true}/>
+                <Input label = "Barangay"  required = {!disabled} msg = {errors.barangay} disabled = {inputDisabled} bind:value = {data.barangay} margin = {true}/>
                 {#if !disabled}
-                <div class = "-ml-4 z-300"> <Check label = "Able to Work" bind:checked = {data.canWork} margin = {true}/> </div>
+                <div class = "z-300"> <Check label = "Able to Work" bind:checked = {data.canWork} margin = {true}/> </div>
                 {/if}
                 {#if data.canWork}
-                    <div class = "-ml-4"> <Select label = "Employment Type" options = {['Self-Employed','Sheltered Workshop','Wage Employed']} disabled = {inputDisabled} bind:value = {data.employmentType} margin = {true}/> </div>
+                    <Select label = "Employment Type" options = {['Self-Employed','Sheltered Workshop','Wage Employed']} disabled = {inputDisabled} bind:value = {data.employmentType} margin = {true}/>
                 {/if}
-                <div class = "-ml-4"> <Select label = "Disability Category" bind:value = {data.disabilityCategoryID} required = {!disabled} options = {discatOptions} msg = {errors.disabilityCat} disabled = {inputDisabled}  margin = {true}/> </div>
-                <div class = "-ml-4"> <Input label = "Disability Nature"  disabled = {inputDisabled} bind:value = {data.disabilityNature} margin = {true}/> </div>
-                <div class = "-ml-4 mt-10"> <Input type = "date" label = "Date of Admission" msg = {errors.admissionDate} required = {!disabled} disabled = {inputDisabled} bind:value = {data.admissionDate} margin = {true}/> </div>
+                <Select label = "Disability Category" bind:value = {data.disabilityCategoryID} required = {!disabled} options = {discatOptions} msg = {errors.disabilityCat} disabled = {inputDisabled}  margin = {true}/>
+                <Input label = "Disability Nature"  disabled = {inputDisabled} bind:value = {data.disabilityNature} margin = {true}/>
+                <div class = "mt-10"> <Input type = "date" label = "Date of Admission" msg = {errors.admissionDate} required = {!disabled} disabled = {inputDisabled} bind:value = {data.admissionDate} margin = {true}/> </div>
         </div>
         <div> 
             <div class = "w-full"> <TextArea disabled = {inputDisabled} value = {data.remarks || "N/A"} label = "Remarks" rows = 10/> </div>
