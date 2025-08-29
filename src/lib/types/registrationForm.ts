ec881251f7e8a30b97e1546dd3820ced211fafe6
bea-nsprout <beaantoinette26@gmail.com>
2025-08-29 23:08:46 +0800

    Updated: registration types

--- src/lib/types/registrationForm.ts
+++ src/lib/types/registrationForm.ts
@@ -72,6 +72,82 @@ export type CaregiverError = {
 	msg: string;			// for existing caregivers
 }
 
+// object type used to store information about a new child in the registration form
+export type NewChild = {
+	first_name: string;
+	middle_name: string;
+	last_name: string;
+	birthday: string;
+	age: string;
+	sex: string;
+	address: string;
+	barangay: string;
+	remarks: string;
+	date_admission: string;
+	disability: {
+		category_id: number | null;
+		nature: string;
+	};
+	education: {
+		type: string;
+		grade_level: string;
+		status: string;
+		year_start: string;
+		year_end: string;
+	}[];
+	has: {
+		birth_cert: boolean;
+		medical_cert: boolean;
+		barangay_cert: boolean;
+		philhealth: boolean;
+		pwd_id: boolean;
+		pwd: {
+			expiry_date: string;
+			id: string;
+		};
+		vote: boolean;
+		national_id: boolean;
+	};
+	employment: {
+		able_to_work: boolean;
+		type: string;
+	};
+	part: {
+		family_life: boolean;
+		fam_year: number;
+		community: boolean;
+		com_year: number;
+	};
+	participation?: {
+		social_protection: boolean;
+		family_life: boolean;
+		community_life: boolean;
+		year: number;
+	}[];
+};
+
+export type ChildError = {
+	overall: string;
+	firstName: string;
+	lastName: string;
+	birthday: string;
+	sex: string;
+	address: string;
+	barangay: string;
+	disCategory: string;
+	disNature: string;
+	educType: string;
+	educLvl: string;
+	educStatus: string;
+	pwdId: string;
+	pwdExpy: string;
+	admissionDate: string;
+	partFamilyYear: string;
+	ayStart: string;
+	ayEnd: string;
+	partCommunityYear: string;
+};
+
 // list of all members who are part of a family (returned from GET api, not really associated to the child yet)
 export type MemberListFamily = InfoLinked & {
 	family_id: string;
